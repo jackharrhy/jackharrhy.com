77834ad666c8ac8d7094ed4f4f2e602e6d4d9423
Jack Arthur Harrhy <jack@jackharrhy.com>
2016-12-17 10:35:20 -0330

    added google analytics

--- src/js/index.js
+++ src/js/index.js
@@ -73,3 +73,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	};
 	render();
 });
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-89083457-1', 'auto');
+ga('send', 'pageview');
