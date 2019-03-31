a41e6c7fd0ab4a194dac3beb00ae17814f9529c4
Jack Arthur Harrhy <me@jackharrhy.com>
2019-03-30 22:24:11 -0230

    hi stoof 2

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './styles/main.scss';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({antialias: true});
 
 const clearColor = CLEARCOLOR;
 
