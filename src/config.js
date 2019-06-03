0249ddf7df586bec0a167886e5b65b00654b2c85
Jack Arthur Harrhy <me@jackharrhy.com>
2019-06-02 22:11:50 -0230

    SS_URI must be valid url to be used

--- src/config.js
+++ src/config.js
@@ -1,9 +1,18 @@
 const ssEndpoint = process.env.SS_ENDPOINT;
 const ssURI = process.env.SS_URI;
 
+const isValidUrl = (string) => {
+  try {
+    new URL(string);
+    return true;
+  } catch (_) {
+    return false;
+  }
+};
+
 let socketServerURI;
 
-if (ssURI === undefined) {
+if (isValidUrl(ssURI)) {
 	socketServerURI = `${window.location.protocol === 'https:' ? 'wss://' : 'ws://'}${window.location.host}` + ssEndpoint;
 }
 else {
