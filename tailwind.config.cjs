9f6e5678f20b18ed911cb77fa55ae7914cb13a6c
Jack Arthur Harrhy <me@jackharrhy.com>
2023-07-25 23:05:21 -0230

    posts infra, start of where the bus post, unpublished

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-	theme: {
-		extend: {
-			colors: {
-				background: "#fdf5ee",
-				link: "#416bce",
-			},
-		},
-	},
-	plugins: [],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#fdf5ee",
+        link: "#416bce",
+      },
+    },
+  },
+  plugins: [],
 };
