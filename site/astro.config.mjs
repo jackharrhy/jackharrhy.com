9f476724a74e2fbf620f39171b19c8a162ca1433
Jack Arthur Harrhy <me@jackharrhy.com>
2025-02-02 19:06:59 -0330

    tools page, tweet archiver wip

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -6,31 +6,8 @@ import mdx from "@astrojs/mdx";
 
 import remarkWikiLink from "remark-wiki-link";
 
-import fsSync from "fs";
-
 const slugify = (filename) =>
   filename.replace(".mdx", "").toLowerCase().replace(/\s+/g, "-");
-
-const getGardenFiles = (dir) => {
-  const files = [];
-  const entries = fsSync.readdirSync(dir, { withFileTypes: true });
-
-  for (const entry of entries) {
-    const fullPath = `${dir}/${entry.name}`;
-    if (entry.isDirectory()) {
-      files.push(...getGardenFiles(fullPath));
-    } else if (entry.isFile() && entry.name.endsWith(".mdx")) {
-      const relativePath = fullPath.replace("src/data/garden/", "");
-      files.push(slugify(relativePath));
-    }
-  }
-
-  return files;
-};
-
-const gardenFiles = getGardenFiles("src/data/garden");
-
-const permalinks = [...gardenFiles, "debug", "404"];
 
 // https://astro.build/config
 export default defineConfig({
@@ -45,7 +22,6 @@ export default defineConfig({
         [
           remarkWikiLink,
           {
-            permalinks,
             pageResolver: (name) => [slugify(name)],
             hrefTemplate: (permalink) =>
               permalink === "home" ? "/" : `/${permalink}`,
